90088e7da798d4a3caf4aae4821193b5c202853b
Wellington <wellington.climaco9@gmail.com>
2025-07-24 21:29:40 -0300

    feat: 100% test coverage in userService

--- SistemaLeilao.Test/UnitTests/Application/Services/UserServiceTests.cs
+++ SistemaLeilao.Test/UnitTests/Application/Services/UserServiceTests.cs
@@ -1,6 +1,7 @@
 using FluentResults;
 using Moq;
 using SistemaLeilao.Application.Interface;
+using SistemaLeilao.Application.Request;
 using SistemaLeilao.Application.Services;
 using SistemaLeilao.Core;
 using SistemaLeilao.Core.Interface;
@@ -48,6 +49,84 @@ public class UserServiceTests
         //act
         var result = await _userService.GetUserByEmail(email);
 
+        //assert
+        Assert.Equal(expected.IsSuccess, result.IsSuccess);
+    }
+    
+    [Fact]
+    public async void DeveRetornarMensagemDeFalhaCasoNaoEncontreUserById()
+    {
+        //arrange
+        var id = Guid.NewGuid();
+        var expected = "Usuário não encontrado";
+        _userRepository.Setup(x => x.GetUserById(id)).ReturnsAsync(() => null);
+        
+        //act
+        var result = await _userService.GetUserById(id);
+        
+        //assert
+        Assert.Equal(expected,result.Errors.FirstOrDefault().Message);
+    }
+
+    [Fact]
+    public async void DeveRetornarResultOkCasoConsigaEncontrarUserById()
+    {
+        //arrange
+        var id = Guid.NewGuid();
+        var usuario = new Usuario("teste", new Email("teste@email.com"));
+        var expected = Result.Ok();
+        _userRepository.Setup(x => x.GetUserById(id)).ReturnsAsync(() => usuario);
+        
+        //act
+        var result = await _userService.GetUserById(id);
+        
+        //assert
+        Assert.Equal(expected.IsSuccess, result.IsSuccess);
+    }
+    
+    [Fact]
+    public async void CreateUserDeveRetornarMensagemDeErroCasoJaExistaUserComEmail()
+    {
+        //arrange
+        var request = new CreateUserRequest("nome", "teste@email.com");
+        var email = new Email(request.email);
+        var usuario = new Usuario("teste", new Email("teste@email.com"));
+
+        var expected = "Usuário com esse email já existe";
+        
+        _userRepository.Setup(x => x.GetUserByEmail(It.IsAny<Email>())).ReturnsAsync(() => usuario);
+
+        //act
+        var result = await _userService.CreateUser(request);
+        
+        //assert
+        Assert.Equal(expected, result.Errors.FirstOrDefault().Message);
+    }
+
+    [Fact]
+    public async void CreateUserDeveThrowExceptionCasoFormatoDeEmailInvalido()
+    {
+        //arrange
+        var request =new CreateUserRequest("nome", "emailInvalido");
+        
+        //act & assert
+        await Assert.ThrowsAsync<ArgumentException>(() => _userService.CreateUser(request));
+    }
+    
+    [Fact]
+    public async void DeveRetornarResultOkCasoConsigaCriarUsuario()
+    {
+        //arrange
+        var request = new CreateUserRequest("nome", "teste@email.com");
+        var expected = Result.Ok();
+        var usuario = new Usuario("teste", new Email("teste@email.com"));
+        
+        _userRepository.Setup(x => x.GetUserByEmail(It.IsAny<Email>())).ReturnsAsync(() => null);
+        _userRepository.Setup(x => x.RegisterUser(It.IsAny<Usuario>())).ReturnsAsync(() => usuario);
+        
+        //act
+        var result = await _userService.CreateUser(request);
+        
         //assert
         Assert.Equal(expected.IsSuccess, result.IsSuccess);
     }
