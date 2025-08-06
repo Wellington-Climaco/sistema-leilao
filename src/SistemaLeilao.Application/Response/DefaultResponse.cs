822ff9b5bb9f5e1561cde2fc8ab5a87482bc59b2
Wellington <wellington.climaco9@gmail.com>
2025-08-05 21:52:40 -0300

    default template response

---
+++ src/SistemaLeilao.Application/Response/DefaultResponse.cs
@@ -0,0 +1,21 @@
+namespace SistemaLeilao.Application.Response;
+
+public class DefaultResponse<T>
+{
+    public DefaultResponse(T data, string statusCode)
+    {
+        Data = data;
+        StatusCode = statusCode;
+    }
+
+    public DefaultResponse(string statusCode, List<string> errors)
+    {
+        StatusCode = statusCode;
+        Errors = errors;
+    }
+  
+    public string StatusCode { get; private set; }
+    public T Data { get; private set; }
+    public List<string> Errors { get; private set; } = new();
+    
+}
